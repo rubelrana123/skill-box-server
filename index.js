ae01d380c23e7992c818ff0a59382bb5389d9933
Md Rubel Rana <96787688+rubelrana123@users.noreply.github.com>
2022-10-26 23:07:40 +0600

    added FAQ json ans create API

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors ());
+const blog = require("./data/blog.json");
+const FQA = require("./data/faq.json")
 app.get("/", (req, res) => {
   res.send("Server is Running")
 });
+app.get("/blog", (req, res) => {
+  res.send(blog);
+})
+app.get("/faq", (req, res) => {
+  res.send(FQA);
+})
 app.listen(port, () => {
   console.log('Server is running port', port);
 })
