1fbcc1fe1080e599c0ef98e1d30bb16c43fcb17d
Md Rubel Rana <96787688+rubelrana123@users.noreply.github.com>
2022-10-26 20:53:27 +0600

    added blog question and create api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 app.use(cors ());
 app.get("/", (req, res) => {
   res.send("Server is Running")
